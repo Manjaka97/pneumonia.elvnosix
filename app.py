d3d97156a6a8582b5ed6e7913e6cfd2b06a86717
Manjaka Andriamasinoro <manjaka.andriamasinoro@gmail.com>
2019-03-16 18:59:47 -0400

    Added functionnal test.html

--- app.py
+++ app.py
@@ -4,19 +4,24 @@ app = Flask(__name__)
 
 from prediction import predict_disease
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
 	if request.method == 'GET':
 		return render_template('index.html')
 
-	if request.method == 'POST':
+@app.route('/test', methods=['GET','POST'])
+def test():
+	if request.method == 'GET':
+		return render_template('test.html')
+
+	if request.method == 'POST'
 		if 'file' not in request.files:
 			print('no file uploaded')
 			return
 		file = request.files['file']
 		image = file.read()
 		prediction = predict_disease(image)
-		return render_template('result.html', prediction=prediction) 
+		return render_template('result.html', prediction=prediction)
 
 
 if __name__ == "__main__":
