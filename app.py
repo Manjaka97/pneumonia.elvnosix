26f69530fc2fcb3dc3f58d12500eaf9f2f64a657
Manjaka Andriamasinoro <manjaka.andriamasinoro@gmail.com>
2019-03-17 00:31:51 -0400

    Added about me page

--- app.py
+++ app.py
@@ -29,6 +29,11 @@ def pneumonia():
 	if request.method == 'GET':
 		return render_template('pneumonia.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+	if request.method == 'GET':
+		return render_template('about.html')
+
 
 if __name__ == "__main__":
   app.run()
